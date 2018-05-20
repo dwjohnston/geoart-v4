aeb05b5da392c41469828556cdf5fa80be894939
David Johnston <david@blacksheepcode.com>
2018-05-20 12:43:42 +1000

    add full color preview to planet preview

--- frontend/src/components/PlanetPreview.js
+++ frontend/src/components/PlanetPreview.js
@@ -5,10 +5,21 @@ class PlanetPreview extends React.Component {
 
   render() {
     return (
-      <div 
-        className="planet-preview" 
-        style = {{"borderColor":  this.props.planet.color.shift(50,0).toString()}} >
-      </div>
+
+      <div className = "planet-preview"> 
+
+        <div/> 
+
+        <div 
+          className="planet-preview-outline" 
+          style = {{"borderColor":  this.props.planet.color.shift(50,0).toString()}} >
+        </div>
+
+        <div 
+          className = "planet-preview-solid-square" 
+          style = {{"backgroundColor": this.props.planet.color.shift(0, 1).toString()}}
+        /> 
+      </div> 
     );
   }
 }
